e23dea1c925d243c9c4cba3afda999c3b5b0a75b
shunyaoyangshunyao <u7341225@anu.edu.au>
2024-10-22 21:08:54 +1100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [gameTitle, setGameTitle] = useState("");
+  const [searchedGames, setSearchedGames] = useState([]);
+  const [gamesDeal, setGamesDeal] = useState([]);
+
+  const searchGame = () => {
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=5`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSearchedGames(data);
+      });
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=20&pageSize=3`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setGamesDeal(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchSection">
+        <h1> Search For A Game </h1>
+        <input
+          type="text"
+          placeholder="100zz"
+          onChange={(event) => {
+            setGameTitle(event.target.value);
+          }}
+        />
+        <button onClick={searchGame}>Search Game Titile</button>
+
+        <div className="games">
+          {searchedGames.map((game, key) => {
+            return (
+              <div className="game" key={key}>
+                {game.external}
+                <img src={game.thumb} />
+                {game.cheapest}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="dealsSection">
+        <h1> Latest Deals </h1>
+        <div className="games">
+          {gamesDeal.map((game, key) => {
+            return (
+              <div className="game" id="deals" key={key}>
+                <h3>{game.title}</h3>
+                <p>Normal Price: {game.normalPrice}</p>
+                <p>SalePrice: {game.salePrice}</p>
+                <h3>You Save {game.savings.substr(0, 2)}%</h3>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
